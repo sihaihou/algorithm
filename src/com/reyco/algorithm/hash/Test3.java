8dbacb1134168cafb0a6dc335028f50c09df97fc
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-10-30 16:40:43 +0800

    Create Test3.java

---
+++ src/com/reyco/algorithm/hash/Test3.java
@@ -0,0 +1,22 @@
+package com.reyco.algorithm.hash;
+
+/**
+ * 有一个包含100一个URL的大文件，假设每个URL占用64B，请找出所有重复的URL.
+ * 答：hash表、布隆过滤器
+ * 
+ * 
+ * 
+ * 补充：某公司一天的用户搜索词汇量是海里的（百亿数据量），请设计一种求出每天热门Top100词汇的可行方法!
+ * 答：
+ * 
+ * 
+ * @author reyco
+ *
+ */
+public class Test3 {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
