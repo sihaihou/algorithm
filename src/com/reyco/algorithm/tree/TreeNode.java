cda2996cc38bc5bf4c9b344d9f1c6e04042f0892
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-10-30 22:10:18 +0800

    Create TreeNode.java

---
+++ src/com/reyco/algorithm/tree/TreeNode.java
@@ -0,0 +1,19 @@
+package com.reyco.algorithm.tree1;
+
+public class TreeNode {
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+	public TreeNode() {
+	}
+	public TreeNode(int val) {
+		super();
+		this.val = val;
+	}
+	public TreeNode(int val, TreeNode left, TreeNode right) {
+		super();
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}
