343d635c69d82391bee6075b5990c84d8c26517e
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-10-30 16:20:38 +0800

    Create Test2.java

---
+++ src/com/reyco/algorithm/bit/Test2.java
@@ -0,0 +1,24 @@
+package com.reyco.algorithm.bit.test1;
+
+/**
+ * 二进制位出现1的次数
+ * @author reyco
+ *
+ */
+public class Test2 {
+	public static void main(String[] args) {
+		int hammingWeight = hammingWeight(11);
+		System.out.println(hammingWeight);
+	}
+	public static int hammingWeight(int n) {
+        int times = 0;
+        int i = 0;
+        while(i<32) {
+        	if((n&(1<<i))!=0) {
+        		times++;
+        	}
+        	i++;
+        }
+        return times;
+    }
+}
