5f315bae27fa01e65fc9a2f4b19232ef1563156c
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-10-30 22:04:34 +0800

    Create Test7.java

---
+++ src/com/reyco/algorithm/link/Test7.java
@@ -0,0 +1,171 @@
+package com.reyco.algorithm.link;
+
+/**
+ * 两个可能有环的链表相交,返回第一个相交的节点
+ * @author reyco
+ *
+ */
+public class Test7 {
+	public static void main(String[] args) {
+		ListNode node9 = new ListNode(9);
+		ListNode node8 = new ListNode(8,node9);
+		ListNode node7 = new ListNode(7,node8);
+		ListNode node6 = new ListNode(6,node7);
+		ListNode node5 = new ListNode(5,node6);
+		ListNode node4 = new ListNode(4,node5);
+		ListNode node3 = new ListNode(3,node4);
+		ListNode node2 = new ListNode(2,node3);
+		
+		ListNode head1 = new ListNode(1);
+		head1.next = node2;
+		node9.next = node6;
+		
+		ListNode head3_1 = new ListNode(3,node4);
+		ListNode node2_1 = new ListNode(3,head3_1);
+		ListNode head2 = new ListNode(1,node2_1);
+		//
+		ListNode insertsectLoop = getInsertsectLoop(head1, head2);
+		if(insertsectLoop!=null) {
+			System.out.println(insertsectLoop.value);
+		}
+	}
+	/**
+	 * 返回两个可能有环链表相交的第一个节点
+	 * @param head1
+	 * @param head2
+	 * @return
+	 */
+	public static ListNode getInsertsectLoop(ListNode head1,ListNode head2) {
+		ListNode loop1 = loop(head1);
+		ListNode loop2 = loop(head2);
+		//两个没有入环的相交
+		if(loop1==null && loop2==null) {
+			return noLoop(head1,head2);
+		}
+		//两个有环的相交
+		if(loop1!=null && loop2!=null) {
+			return bothLoop(head1,loop1,head2,loop2);
+		}
+		//一个有环，一个没环，绝对不相交
+		return null;
+	}
+	/**
+	 * 两个都有环的链表相交的第一个节点
+	 * @param head1  
+	 * @param loop1
+	 * @param head2
+	 * @param loop2
+	 * @return
+	 */
+	private static ListNode bothLoop(ListNode head1, ListNode loop1, ListNode head2, ListNode loop2) {
+		ListNode curr1 = null;
+		ListNode curr2 = null;
+		if(loop1 == loop2) {
+			curr1 = head1;
+			curr2 = head2;
+			int len = 0;
+			while(curr1!=loop1) {
+				curr1 = curr1.next;
+				len++;
+			}
+			while(curr2!=loop2) {
+				curr2 = curr2.next;
+				len--;
+			}
+			curr1 = len > 0 ? head1 : head2;
+			curr2 = curr1==head1 ? head2 : head1;
+			len = Math.abs(len);
+			while(len!=0) {
+				curr1 = curr1.next;
+				len--;
+			}
+			while(curr1!=curr2) {
+				curr1 = curr1.next;
+				curr2 = curr2.next;
+			}
+			return curr1;
+		}else{
+			curr1 = loop1.next;
+			while(curr1!=loop1) {
+				if(curr1==loop2) {
+					return curr1;
+				}
+				curr1 = curr1.next;
+			}
+			return null;
+		}
+	}
+	/**
+	 * 没有环的两个链表相交的第一个节点
+	 * @param head1
+	 * @param head2
+	 * @return
+	 */
+	private static ListNode noLoop(ListNode head1, ListNode head2) {
+		int len = 0;
+		ListNode curr1 = head1;
+		ListNode curr2 = head2;
+		while(curr1.next!=null) {
+			curr1 = curr1.next;
+			len++;
+		}
+		while(curr2.next!=null) {
+			curr2 = curr2.next;
+			len--;
+		}
+		curr1 = len > 0 ? head1 : head2;
+		curr2 = curr1==head1 ? head2 : head1;
+		len = Math.abs(len);
+		while(len>0) {
+			curr1 = curr1.next;
+			len--;
+		}
+		while(curr1.next!=null) {
+			if(curr1==curr2) {
+				return curr1;
+			}
+			curr1 = curr1.next;
+			curr2 = curr2.next;
+		}
+		return null;
+	}
+	/**
+	 * 找到链表的第一个入环点,如果没有返回null
+	 * @param head
+	 * @return
+	 */
+	public static ListNode loop(ListNode head) {
+		if(head==null || head.next==null || head.next.next==null) {
+			return null;
+		}
+		ListNode slowNode = head.next;
+		ListNode quickNode = head.next.next;
+		while(slowNode!=quickNode) {
+			if(quickNode.next==null || quickNode.next.next==null) {
+				return null;
+			}
+			slowNode = slowNode.next;
+			quickNode = quickNode.next.next;
+		}
+		quickNode = head;
+		while(slowNode != quickNode) {
+			slowNode = slowNode.next;
+			quickNode = quickNode.next;	
+		}
+		return slowNode;
+	}
+	
+	public static class ListNode{
+		int value;
+		ListNode next;
+		public ListNode() {
+		}
+		ListNode(int value) {
+			this.value = value;
+		}
+		ListNode(int value, ListNode next) {
+			this.value = value;
+			this.next = next;
+		}
+	}
+}
