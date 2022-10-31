385c17d140af0ff98445f8a7366b14dfab5b5494
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-10-31 21:30:19 +0800

    Create Test3.java

---
+++ src/com/reyco/algorithm/gameTheory/Test3.java
@@ -0,0 +1,19 @@
+package com.reyco.algorithm.gameTheory;
+
+/**
+ * 给定一个非负数组arr。arr[i]代表该位置上有几个硬币，a和b玩游戏，a先手，b后手，轮到某个人的时候，只能在一个位置上拿任意枚硬币，但是不能不拿，谁最先把硬币拿完谁赢。
+ * 答：所有数异或和等于0，后手赢;不等于0，先手赢。
+ * @author reyco
+ *
+ */
+public class Test3 {
+
+
+	public static void main(String[] args) {
+		
+	}
+	public static boolean ways(int[] arr) {
+		return false;
+	}
+	
+}
