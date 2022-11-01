9233d991c453a71db445ebbc52bed0bb335d12fa
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-11-01 20:52:53 +0800

    Create Test3.java

---
+++ src/com/reyco/algorithm/string/array1/Test3.java
@@ -0,0 +1,14 @@
+package com.reyco.lgorithm.string.array1;
+
+/**
+ * 给定字符串str1和str2，求str1的子串中包含str2所有字符的最小子串长度。
+ * 例：
+ * 	 str1="abcde" str2="ac" 'abc'包含str2的所有字符，长度3.
+ * 	 str1="12345" str2="344" 最小包含子串不存在，长度0
+ * 答：从左到右，滑动窗口+词频
+ * @author reyco
+ *
+ */
+public class Test3 {
+
+}
