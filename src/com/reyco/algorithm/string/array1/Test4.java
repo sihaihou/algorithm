cf9e13a5fb7be5d802f939d7eb2b938473651055
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-11-01 20:53:15 +0800

    Create Test4.java

---
+++ src/com/reyco/algorithm/string/array1/Test4.java
@@ -0,0 +1,40 @@
+package com.reyco.lgorithm.string.array1;
+
+/**
+ * 正数数组arr,求数组中所有元素相加和等于k的最长子数组长度。
+ * @author reyco
+ *
+ */
+public class Test4 {
+	public static void main(String[] args) {
+		int[] arr = {6,2,3,1,1,1,1};
+		int maxLength = maxLength(arr,6);
+		System.out.println(maxLength);
+	}
+	public static int maxLength(int[] arr,int k) {
+		if(arr==null || arr.length==0 || k<0) {
+			return 0;
+		}
+		int maxLength = 0;
+		int left = 0;
+		int right = 1;
+		int sum = arr[0];
+		while(right<arr.length && left<=right) {
+			if(sum==k) {
+				maxLength = Math.max(right-left, maxLength);
+				if(right==arr.length-1) {
+					break;
+				}
+				sum += arr[right];
+				right++;
+			}else if(sum>k) {
+				sum -= arr[left];
+				left++;
+			}else if(sum<k){
+				sum += arr[right];
+				right++;
+			}
+		}
+		return maxLength;
+	}
+}
