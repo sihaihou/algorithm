589cb738cc6f7beda9785d4b54a122ad316c71c6
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-11-01 21:15:21 +0800

    Create Test3.java

---
+++ src/com/reyco/algorithm/test/test4/Test3.java
@@ -0,0 +1,48 @@
+package com.reyco.algorithm.test.test4;
+
+import java.util.Stack;
+
+/**
+ * 反转栈--逆序栈
+ * @author reyco
+ *
+ */
+public class Test3 {
+	
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<>();
+		stack.push(3);
+		stack.push(2);
+		stack.push(1);
+		while(!stack.isEmpty()) {
+			System.out.println(stack.pop());
+		}
+		stack.push(3);
+		stack.push(2);
+		stack.push(1);
+		reverse(stack);
+		while(!stack.isEmpty()) {
+			System.out.println(stack.pop());
+		}
+	}
+	
+	public static void reverse(Stack<Integer> stack) {
+		if(stack.isEmpty()) {
+			return;
+		}
+		int i = f(stack);
+		reverse(stack);
+		stack.push(i);
+	}
+	
+	public static int f(Stack<Integer> stack) {
+		int result = stack.pop();
+		if(stack.isEmpty()) {
+			return result;
+		}
+		int last = f(stack);
+		stack.push(result);
+		return last;
+	}
+	
+}
