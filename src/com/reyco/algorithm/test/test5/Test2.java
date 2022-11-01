827f0406ee7d0028e2d77c24d8fc13e3b24b79d6
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-11-01 21:17:48 +0800

    Create Test2.java

---
+++ src/com/reyco/algorithm/test/test5/Test2.java
@@ -0,0 +1,23 @@
+package com.reyco.algorithm.test.test5;
+
+/**
+ * A（1）、B（2）、C（3）...Z(26)、AA(27)、AB（28）...
+ * 实现根据对应关系完成字符串和数字的互相转换.
+ * 答：26伪进制 ：如十进制100用伪7进制表示：172（1：1*(7^2)=49、 7：1*(7^1) 2:2*(7^0)次方.
+ * @author reyco
+ *
+ */
+public class Test2 {
+	
+	public static void main(String[] args) {
+		
+	}
+	public static int stringToNumer(String s) {
+	
+		return 0;
+	}
+	public static String numberToString(int number) {
+		return "";
+	}
+	
+}
