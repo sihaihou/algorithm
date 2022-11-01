e16b2b47b0c856c8b81d421d206567f8c80de331
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-11-01 20:56:04 +0800

    Create Test1.java

---
+++ src/com/reyco/algorithm/string/array2/Test1.java
@@ -0,0 +1,25 @@
+package com.reyco.lgorithm.string.array2;
+
+/**
+ * 返回子数组的最大累加和
+ * @author reyco
+ *
+ */
+public class Test1 {
+
+	public static void main(String[] args) {
+		int[] nums = {1,2,-1,-2,4,-5,8,9,-1,3};
+		int maxSum = maxSum(nums);
+		System.out.println(maxSum);
+	}
+	public static int maxSum(int[] nums) {
+		int curr = 0;
+		int max = Integer.MIN_VALUE;
+		for (int i : nums) {
+			curr += i;
+			max = Math.max(max, curr);
+			curr = curr<0 ? 0 : curr;
+		}
+		return max;
+	}
+}
