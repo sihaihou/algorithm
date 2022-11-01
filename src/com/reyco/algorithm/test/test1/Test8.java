545132dcfb7e3595e1fec8dbc7e1beddf49d2e12
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-11-01 21:02:57 +0800

    Create Test8.java

---
+++ src/com/reyco/algorithm/test/test1/Test8.java
@@ -0,0 +1,17 @@
+package com.reyco.algorithm.test.test1;
+
+/**
+ * ()()()的深度是1，((()))的深度是3。
+ * 给你一个合法的括号序列，计数深度？
+ * 答：一个count属性，遇到左括号，count++,遇到右括号，count--，取count出现的最大值就是最大深度
+ * @author reyco
+ *
+ */
+public class Test8 {
+
+public static void main(String[] args) {
+		
+		
+	}
+	
+}
