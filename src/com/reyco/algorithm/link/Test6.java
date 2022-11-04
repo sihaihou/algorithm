916731d3d89ad51fca1f67e9b748dc0de6872eb1
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-11-04 21:48:37 +0800

    Update Test6.java

--- src/com/reyco/algorithm/link/Test6.java
+++ src/com/reyco/algorithm/link/Test6.java
@@ -2,6 +2,11 @@ package com.reyco.algorithm.link;
 
 /**
  * 判断链表是否回文结构
+ * 答：
+ *  1）快慢指针先找到下中节点;
+ *  2）然后反转后半部分链表结构；
+ *  3）双指针从头和下中节点遍历是否相等,只要有一个不等，否回文链表；否则是回文链表。
+ *  4）恢复反转后的后半部分链表结构。
  * @author reyco
  *
  */
@@ -27,13 +32,14 @@ public class Test6 {
 		if(head==null || head.next==null) {
 			return true;
 		}
+		//快慢指针先找到下中节点;
 		ListNode slow = head.next;
 		ListNode fast = head.next;
 		while(fast.next!=null && fast.next.next!=null) {
 			slow = slow.next;
 			fast = fast.next.next;
 		}
-		//反正后半部分
+		//然后反转后半部分链表结构；
 		ListNode pre = null;
 		ListNode next = null;
 		ListNode curr = slow;
@@ -43,7 +49,7 @@ public class Test6 {
 			pre = curr;
 			curr = next;
 		}
-		//判断是否回文
+		//双指针从头和下中节点遍历是否相等,只要有一个不等，否回文链表；否则是回文链表。
 		boolean flag = true;
 		ListNode curr1 = pre;
 		ListNode curr2 = head;
@@ -55,7 +61,7 @@ public class Test6 {
 			curr1 =curr1.next;
 			curr2 =curr2.next;
 		}
-		//还原链表
+		//恢复反转后的后半部分链表结构。
 		ListNode preNode = pre;
 		pre = null;
 		next = null;
