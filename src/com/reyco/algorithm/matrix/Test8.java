952152028628b74e0c98c33904e96e8feda85356
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-11-06 00:06:16 +0800

    Update Test8.java

--- src/com/reyco/algorithm/matrix/Test8.java
+++ src/com/reyco/algorithm/matrix/Test8.java
@@ -8,6 +8,7 @@ import java.util.TreeMap;
 import java.util.Map.Entry;
 
 /**
+ * 
  * 给定一个N*3的矩阵matrix，对于每一个长度为3的小数组arr,都表示一个大楼的三个数据,arr[0]表示大楼的左边界，arr[1]表示大楼的右边界，arr[2]表示大楼的高度；
  * 每座大楼的地基都在X轴上，大楼之间可能会有重叠，请返回整理的轮廓线数据？
  * 答：例：{
@@ -30,7 +31,8 @@ import java.util.Map.Entry;
  *    	{11,del,9},
  *    	{14,del,4},
  *    }
- *    第二步：根据Node数组，生成heightTimesMap和xHeightMap
+ *    第二步：通过高度词频获取每个位置的最大高度
+ *    第三步：根据最大高度变化生成大楼轮廓。
  * @author reyco
  *
  */
@@ -48,6 +50,7 @@ public class Test8 {
 	}
 	
 	public static List<List<Integer>> buildContourLine(int[][] matrix){
+		//1,生成Node数组: node的x相等，add类型放前面：
 		Node[] arr = new Node[matrix.length<<1];
 		for(int i=0;i<matrix.length;i++) {
 			arr[i*2] = new Node(matrix[i][0],true, matrix[i][2]);
@@ -55,11 +58,11 @@ public class Test8 {
 		}
 		Arrays.sort(arr, new NodeComparator());
 		//print(arr);
-		// key:高度h
-		// value:出现的次数
+		
+		//2,通过高度词频获取每个位置的最大高度
+		// key:高度h	value:出现的次数
 		TreeMap<Integer,Integer> heightTimesMap = new TreeMap<>();
-		// key:x轴
-		// value： 高度
+		// key:x轴    value： 高度
 		TreeMap<Integer,Integer> xHeightMap = new TreeMap<>();
 		for (int i=0;i<arr.length;i++) {
 			Node curr = arr[i];
@@ -86,7 +89,7 @@ public class Test8 {
 				xHeightMap.put(curr.x, heightTimesMap.lastKey());
 			}
 		}
-		//拼装轮廓
+		//3,根据最大高度变化生成大楼轮廓。
 		List<List<Integer>> res = new ArrayList<>();
 		int start = 0;
 		int prevHeiht = 0;
