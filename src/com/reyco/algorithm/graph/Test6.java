4c18bdcd86ab527622cbae83c5e94b3d79f59622
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-11-08 22:53:55 +0800

    Update Test6.java

--- src/com/reyco/algorithm/graph/Test6.java
+++ src/com/reyco/algorithm/graph/Test6.java
@@ -18,35 +18,42 @@ public class Test6 {
 		
 	}
 	public static Map<Node, Integer> dijkstra(Node from) {
-		HashMap<Node, Integer> distanceMap = new HashMap<>();
+		//从head出发到所有点的距离
+		Map<Node, Integer> distanceMap = new HashMap<>();
 		distanceMap.put(from, 0);
-		// 打过对号的点
+		//已经选择过的节点，防止重复
 		HashSet<Node> selectedNodes = new HashSet<>();
+		//获取distanceMap中最小距离的记录（不在已选择过的节点中），
 		Node minNode = getMinDistanceAndUnselectedNode(distanceMap, selectedNodes);
 		while (minNode != null) {
 			//  原始点  ->  minNode(跳转点)   最小距离distance
 			int distance = distanceMap.get(minNode);
 			for (Edge edge : minNode.edges) {
 				Node toNode = edge.to;
+				//没有新增，如果已经存在，并且距离更小了，就更新
 				if (!distanceMap.containsKey(toNode)) {
 					distanceMap.put(toNode, distance + edge.weight);
 				} else { // toNode 
 					distanceMap.put(edge.to, Math.min(distanceMap.get(toNode), distance + edge.weight));
 				}
 			}
+			//添加到已选择过集合中
 			selectedNodes.add(minNode);
+			//重新获取最小的，周而复始
 			minNode = getMinDistanceAndUnselectedNode(distanceMap, selectedNodes);
 		}
 		return distanceMap;
 	}
 
-	public static Node getMinDistanceAndUnselectedNode(HashMap<Node, Integer> distanceMap, HashSet<Node> touchedNodes) {
+	public static Node getMinDistanceAndUnselectedNode(Map<Node, Integer> distanceMap, HashSet<Node> touchedNodes) {
+		//当前最小距离距离的点
 		Node minNode = null;
+		//当前最小距离的距离
 		int minDistance = Integer.MAX_VALUE;
 		for (Entry<Node, Integer> entry : distanceMap.entrySet()) {
 			Node node = entry.getKey();
 			int distance = entry.getValue();
-			if (!touchedNodes.contains(node) && distance < minDistance) {
+			if (!touchedNodes.contains(node) && distance < minDistance) { //不在已选择过的中
 				minNode = node;
 				minDistance = distance;
 			}
