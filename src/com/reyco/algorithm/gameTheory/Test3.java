43537623dbc8adc66fad976c0d520868e23f582a
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-11-09 23:06:08 +0800

    Update Test3.java

--- src/com/reyco/algorithm/gameTheory/Test3.java
+++ src/com/reyco/algorithm/gameTheory/Test3.java
@@ -1,6 +1,7 @@
 package com.reyco.algorithm.gameTheory;
 
 /**
+ * 61
  * 给定一个非负数组arr。arr[i]代表该位置上有几个硬币，a和b玩游戏，a先手，b后手，轮到某个人的时候，只能在一个位置上拿任意枚硬币，但是不能不拿，谁最先把硬币拿完谁赢。
  * 答：所有数异或和等于0，后手赢;不等于0，先手赢。
  * @author reyco
