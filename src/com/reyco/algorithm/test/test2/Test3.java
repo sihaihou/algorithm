57af0097b324a237c2c77d2ad06609a0022ae4aa
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-11-09 22:51:13 +0800

    Update Test3.java

--- src/com/reyco/algorithm/test/test2/Test3.java
+++ src/com/reyco/algorithm/test/test2/Test3.java
@@ -1,6 +1,7 @@
 package com.reyco.algorithm.test.test2;
 
 /**
+ * 52
  * 给定 n个非负整数表示每个宽度为 1的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
  * 例如：[0,1,0,2,1,0,1,3,2,1,2,1] 输出：6
  * 答：双指针从最左和最右开始，左右两边谁的最大值最小谁先结算。arr[i] = Math.max(0,Math.min(leftMax,rightMax)-arr[i])
