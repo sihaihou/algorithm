c3d4962afd08fb080e7589e7bd1436c338970c80
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-11-10 22:15:55 +0800

    Update Test5.java

--- src/com/reyco/algorithm/string/array1/Test5.java
+++ src/com/reyco/algorithm/string/array1/Test5.java
@@ -1,8 +1,12 @@
-package com.reyco.lgorithm.string.array1;
+package com.reyco.algorithm.string.array1;
 
 /**
  * 61
  * 数组arr,有正、有负、有零,求数组中所有元素相加和小于或等于k的最长子数组长度。
+ *  答： 
+ *     第一步：生成minSumArr,minSumArrEnd 例: arr[1,2,5,-4,3,-1], minSumArr[1,2,1,-4,2,-1], minSumArrEnd[0,1,3,3,5,5]  
+ *	          minSumArr[i]表示从i出发到右的所有情况中，能得到的最小累加和。
+ *     第二步：      
  * @author reyco
  *
  */
@@ -33,15 +37,17 @@ public class Test5 {
 		int sum = 0;
 		int end = 0;
 		int maxLength = 0;
+		//i是窗口最左的位置，end是是窗口最右位置的下一个位置
 		for (int i=0;i<arr.length;i++) {
+			//以i开头的情况下，累计和<=k的最长子数组是arr[i...end-1],看看这个数组长度能不能更新maxLength
 			while(end < arr.length && sum + minSumArr[end] <= k) {
 				sum += minSumArr[end];
 				end = minSumArrEnd[end]+1;
 			}
 			maxLength = Math.max(end-i,maxLength);
-			if(end>i) {
-				sum += arr[i];
-			}else {
+			if(end>i) {     //窗口还有数
+				sum -= arr[i];
+			}else {  //窗口没有数了，说明从i开头的所有子数组累加和都不可能<=k
 				end = i+1;
 			}
 		}
