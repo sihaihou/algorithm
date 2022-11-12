eb02b9abafc66c692c29ca6d031bf8e38ceeda9b
sihaihou <48390059+sihaihou@users.noreply.github.com>
2022-11-12 14:03:54 +0800

    Update Test8.java

--- src/com/reyco/algorithm/string/array1/Test8.java
+++ src/com/reyco/algorithm/string/array1/Test8.java
@@ -1,4 +1,4 @@
-package com.reyco.lgorithm.string.array1;
+package com.reyco.algorithm.string.array1;
 
 /**
  * 63
@@ -7,10 +7,10 @@ package com.reyco.lgorithm.string.array1;
  *	   str[i..j]上最长回文子序列。dp[i][j]表示str[i~j]上最长回文子序列长度。
  *     求的点就是dp[0][str.length-1]
  *     
- *     1)不i，不j：dp[i-1][j-1]
+ *     1)不i，不j：dp[i+1][j-1]
  *     2)以i，不j：dp[i][j-1]
- *     3)不i，以j：dp[i-1][j]
- *     4)str[i]==str[j] 以i，以j：dp[i][j]+2 
+ *     3)不i，以j：dp[i+1][j]
+ *     4)str[i]==str[j] 以i，以j：dp[i+1][j-1]+2 
  *     四种情况求最大值
  * @author reyco
  *
