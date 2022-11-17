9877aea4734c9f10d97c4a5b697a03a33115bd8a
housihai <48390059+sihaihou@users.noreply.github.com>
2022-11-17 22:50:27 +0800

    Create Test4.java

---
+++ src/com/reyco/algorithm/test/test5/Test4.java
@@ -0,0 +1,12 @@
+package com.reyco.algorithm.test.test5;
+
+/**
+ * 57
+ * a（1）、b（2）、c（3）...z(26)、ab(27)、ac（28）..对于任意长度不超过16的升序字符串，计算出它在上述字典中的编码。
+ * 
+ * @author reyco
+ *
+ */
+public class Test4 {
+	
+}
