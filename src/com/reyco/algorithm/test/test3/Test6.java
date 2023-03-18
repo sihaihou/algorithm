30120e3fda9699fcc0a85624166a98d4cd05c00c
housihai <48390059+sihaihou@users.noreply.github.com>
2023-03-18 21:29:10 +0800

    Update Test6.java

--- src/com/reyco/algorithm/test/test3/Test6.java
+++ src/com/reyco/algorithm/test/test3/Test6.java
@@ -6,7 +6,6 @@ package com.reyco.algorithm.test.test3;
  * 给定一个正数bag,表示一个载重bag的袋子，
  * 你装的物品不能超过这个重量。
  * 返回你能装下最多的价值是多少？
- * 答: 每一个位置的货物要或者不要，求最大值。
  * @author reyco
  *
  */
@@ -17,11 +16,11 @@ public class Test6 {
 		RandomArrayFactory.print(weight);
 		RandomArrayFactory.print(values);
 		int bag = 10;
-		int max = max(weight, values, bag);
+		int max = max1(weight, values, bag);
 		System.out.println(max);
-		int max1 = max1(weight, values, bag);
+		int max1 = max2(weight, values, bag);
 		System.out.println(max1);
-		int max2 = max2(weight, values, bag);
+		int max2 = max3(weight, values, bag);
 		System.out.println(max2);
 	}
 	/**
@@ -31,21 +30,31 @@ public class Test6 {
 	 * @param bag
 	 * @return
 	 */
-	public static int max(int[] weight,int[] values,int bag) {
-		return process(weight, values, 0, bag);
+	public static int max1(int[] weight,int[] values,int bag) {
+		return process1(weight, values, 0, bag);
 	}
-	public static int process(int[] weight,int[] values,int index,int rest) {
+	/**
+	 * @param weight
+	 * @param values
+	 * @param index 当前来到的位置
+	 * @param rest	包裹剩余载重
+	 * @return
+	 */
+	public static int process1(int[] weight,int[] values,int index,int rest) {
 		if(rest<=0) {
 			return 0;
 		}
 		if(index==weight.length) {
 			return 0;
 		}
-		int p1 = process(weight, values, index+1, rest);
+		//不要
+		int p1 = process1(weight, values, index+1, rest);
+		//要
 		int p2 = Integer.MIN_VALUE;
 		if(rest>=weight[index]) {
-			p2 = values[index]+process(weight, values, index+1, rest-weight[index]);
+			p2 = values[index]+process1(weight, values, index+1, rest-weight[index]);
 		}
+		//取最好的
 		return Math.max(p1, p2);
 	}
 	/**
@@ -55,31 +64,26 @@ public class Test6 {
 	 * @param bag
 	 * @return
 	 */
-	public static int max1(int[] weight,int[] values,int bag) {
+	public static int max2(int[] weight,int[] values,int bag) {
 		int[][] dp = new int[weight.length][bag+1];
 		for (int i=0;i<dp.length;i++) {
 			for(int j=0;j<dp[0].length;j++) {
 				dp[i][j] = -1;
 			}
 		}
-		return process1(weight, values, 0, bag,dp);
+		return process2(weight, values, 0, bag,dp);
 	}
-	public static int process1(int[] weight,int[] values,int index,int rest,int[][] dp) {
+	public static int process2(int[] weight,int[] values,int index,int rest,int[][] dp) {
+		if(rest<=0 || index==weight.length) {
+			return 0;
+		}
 		if(dp[index][rest]!=-1) {
 			return dp[index][rest];
 		}
-		if(rest<=0) {
-			dp[index][rest] = 0;
-			return dp[index][rest];
-		}
-		if(index==weight.length) {
-			dp[index][rest] = 0;
-			return dp[index][rest];
-		}
-		int p1 = process(weight, values, index+1, rest);
+		int p1 = process2(weight, values, index+1, rest,dp);
 		int p2 = Integer.MIN_VALUE;
 		if(rest>=weight[index]) {
-			p2 = values[index]+process(weight, values, index+1, rest-weight[index]);
+			p2 = values[index]+process2(weight, values, index+1, rest-weight[index],dp);
 		}
 		dp[index][rest] = Math.max(p1, p2);
 		return dp[index][rest];
@@ -91,7 +95,7 @@ public class Test6 {
 	 * @param bag
 	 * @return
 	 */
-	public static int max2(int[] weight,int[] values,int bag) {
+	public static int max3(int[] weight,int[] values,int bag) {
 		int[][] dp = new int[weight.length+1][bag+1];
 		for(int index=weight.length-1;index>=0;index--) {
 			for(int rest=0;rest<dp[0].length;rest++) {
@@ -103,6 +107,6 @@ public class Test6 {
 				dp[index][rest] = Math.max(p1, p2);
 			}
 		}
-		return process(weight, values, 0, bag);
+		return dp[0][bag];
 	}
 }
